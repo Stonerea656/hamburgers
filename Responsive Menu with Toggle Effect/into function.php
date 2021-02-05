a2b2265bf1953718d3518d6ba577afeff5260824
Gaz <12005337+Stonerea656@users.noreply.github.com>
2021-02-05 12:44:01 +1100

    added components

---
+++ Responsive Menu with Toggle Effect/into function.php
@@ -0,0 +1,2 @@
+wp_enqueue_script( 'wpb_togglemenu', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20160909', true );
+
---
+++ Responsive Menu with Toggle Effect/into header.php
@@ -0,0 +1,4 @@
+<nav id="site-navigation" class="main-navigation" role="navigation">
+            <button class="menu-toggle">Menu</button>
+            <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+</nav>
